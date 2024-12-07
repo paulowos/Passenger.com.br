ec22c37980afab5e43b3b63389b43955a24a32bd
paulowos <paulowos@gmail.com>
2024-12-07 17:36:31 -0300

    Mudança para os elementos se moverem ao abrir o teclado

--- app/src/main/java/br/com/passenger/MainActivity.kt
+++ app/src/main/java/br/com/passenger/MainActivity.kt
@@ -7,8 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -38,7 +42,9 @@ class MainActivity : ComponentActivity() {
                         modifier =
                             Modifier
                                 .fillMaxSize()
-                                .padding(horizontal = 16.dp),
+                                .padding(horizontal = 16.dp)
+                                .windowInsetsPadding(WindowInsets.navigationBars)
+                                .imePadding(),
                     ) {
                         NavHost(
                             navController = navController,
