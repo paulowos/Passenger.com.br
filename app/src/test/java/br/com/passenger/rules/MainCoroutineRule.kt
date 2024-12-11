c58afd298bd633cca119927193873ccb6d18ed9d
paulowos <paulowos@gmail.com>
2024-12-11 19:34:12 -0300

    Criado Rule para testar "viewmodelScope.launch"

--- app/src/test/java/br/com/passenger/rules/MainCoroutineRule.kt
+++ app/src/test/java/br/com/passenger/rules/MainCoroutineRule.kt
@@ -1,33 +1,25 @@
 package br.com.passenger.rules
-//
-// import kotlinx.coroutines.ExperimentalCoroutinesApi
-// import kotlinx.coroutines.test.TestDispatcher
-// import kotlinx.coroutines.test.UnconfinedTestDispatcher
-// import org.junit.rules.TestWatcher
-//
-// @ExperimentalCoroutinesApi
-// class MainCoroutineRule(
-//    val dispatcher: TestDispatcher = UnconfinedTestDispatcher(),
-// ) : TestWatcher() {
-//    override fun starting(description: Description?) {
-//        super.starting(description)
-//        Dispatchers.setMain(dispatcher)
-//    }
-//
-//    override fun finished(description: Description?) {
-//        super.finished(description)
-//        Dispatchers.resetMain()
-//    }
-// }
-//
-// class MainDispatcherRule(
-//    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
-// ) : TestWatcher() {
-//    override fun starting(description: Description) {
-//        Dispatchers.setMain(testDispatcher)
-//    }
-//
-//    override fun finished(description: Description) {
-//        Dispatchers.resetMain()
-//    }
-// }
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@ExperimentalCoroutinesApi
+class MainCoroutineRule(
+    private val dispatcher: TestDispatcher = UnconfinedTestDispatcher(),
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description?) {
+        super.finished(description)
+        Dispatchers.resetMain()
+    }
+}
