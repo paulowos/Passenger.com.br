366331f74972a587410ec0cfa0592f5c013c9710
paulowos <paulowos@gmail.com>
2024-12-11 10:08:49 -0300

    Teste AppModule

---
+++ app/src/test/java/br/com/passenger/AppModuleUnitTest.kt
@@ -0,0 +1,32 @@
+package br.com.passenger
+
+import br.com.passenger.data.network.RideAPI
+import br.com.passenger.data.repository.MapRepository
+import br.com.passenger.data.repository.RideRepository
+import br.com.passenger.di.AppModule
+import org.junit.Test
+import strikt.api.expectThat
+import strikt.assertions.isA
+
+class AppModuleUnitTest {
+    @Test
+    fun `Teste provideRideAPI`() {
+        val rideAPI = AppModule.provideRideAPI()
+
+        expectThat(rideAPI).isA<RideAPI>()
+    }
+
+    @Test
+    fun `Teste provideRideRepository`() {
+        val rideRepository = AppModule.provideRideRepository(AppModule.provideRideAPI())
+
+        expectThat(rideRepository).isA<RideRepository>()
+    }
+
+    @Test
+    fun `Teste provideMapRepository`() {
+        val mapRepository = AppModule.provideMapRepository()
+
+        expectThat(mapRepository).isA<MapRepository>()
+    }
+}
